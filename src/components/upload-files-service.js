9165af497364d9ef007bb6ab492cdaf7c52cecc0
Suyash Patil <suyap18@gmail.com>
2022-04-25 02:02:50 +0530

    commit message

---
+++ src/components/upload-files-service.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+//import http from "../http-common";
+
+class UploadFilesService {
+  upload(file, onUploadProgress) {
+    let formData = new FormData();
+
+    formData.append("file", file);
+
+    return axios.post("/upload", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      onUploadProgress,
+    });
+  }
+
+  getFiles() {
+    return axios.get("/files");
+  }
+}
+
+export default new UploadFilesService();
